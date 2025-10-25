748b839bacbb7a9cc83eb838a46914cb2570e7e7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-10-25 18:54:50 +0530

    add comments and fix a var ps0 problem 6

--- problem-sets/problem-set-0/ps0-template/count_long_subarray.py
+++ problem-sets/problem-set-0/ps0-template/count_long_subarray.py
@@ -3,24 +3,40 @@ def count_long_subarray(A):
     Input:  A     | Python Tuple of positive integers
     Output: count | number of longest increasing subarrays of A
     """
+
+    # Current seq only consists of A[0]
     n = len(A)
     curr_seq_length = 1
-    curr_max = A[0]
+
+    # It is also the max sequence length so far
     max_length = 1
     count = 1
 
+    # For each of the remaining elements
     for i in range(1, n):
-        num = A[i]
-        if num > curr_max:
+        prev, curr = A[i - 1], A[i]
+
+        # If the curr > prev
+        # it means the sequence is extended
+        # So we increment curr_seq_length
+        if curr > prev:
             curr_seq_length += 1
-            curr_max = num
+        # If the curr <= prev, it means that
+        # it is the start of a new sequence
+        # So we update the curr_seq_length to 1
         else:
             curr_seq_length = 1
-            curr_max = num
 
+        # If the curr_seq_length is greater than the max_length
+        # it means we have found a new max length
+        # so we update max_length and the count would be 1
+        # as this is the first such sequence
         if curr_seq_length > max_length:
             max_length = curr_seq_length
             count = 1
+        # If the curr_seq_length is equal to the max_length
+        # it means we found another sequence with the max_length
+        # so we increment the count
         elif curr_seq_length == max_length:
             count += 1
 
