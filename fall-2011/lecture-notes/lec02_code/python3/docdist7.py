0ab5b8fed89084bd6b7b2a9e6d0d6d18c6a9ed6d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-12-13 17:38:01 +0530

    convert python2 to python 3

--- fall-2011/lecture-notes/lec02_code/python3/docdist7.py
+++ fall-2011/lecture-notes/lec02_code/python3/docdist7.py
@@ -6,7 +6,7 @@
 #
 # Usage:
 #    docdist7.py filename1 filename2
-#     
+#
 # This program computes the "distance" between two text files
 # as the angle between their word frequency vectors (in radians).
 #
@@ -29,27 +29,28 @@
 #    norm(x) = sqrt(inner_product(x,x))
 
 import math
-    # math.acos(x) is the arccosine of x.
-    # math.sqrt(x) is the square root of x.
-
 import string
-
 import sys
+
+# math.acos(x) is the arccosine of x.
+# math.sqrt(x) is the square root of x.
+
 
 ##################################
 # Operation 1: read a text file ##
 ##################################
 def read_file(filename):
-    """ 
+    """
     Read the text file with the given filename;
     return a list of the lines of text in the file.
     """
     try:
-        f = open(filename, 'r')
+        f = open(filename, "r")
         return f.readlines()
     except IOError:
-        print "Error opening or reading input file: ",filename
+        print("Error opening or reading input file: ", filename)
         sys.exit()
+
 
 #################################################
 # Operation 2: split the text lines into words ##
@@ -67,10 +68,14 @@ def get_words_from_line_list(L):
         word_list.extend(words_in_line)
     return word_list
 
+
 # global variables needed for fast parsing
 # translation table maps upper case to lower case and punctuation to spaces
-translation_table = string.maketrans(string.punctuation+string.uppercase,
-                                     " "*len(string.punctuation)+string.lowercase)
+translation_table = string.maketrans(
+    string.punctuation + string.uppercase,
+    " " * len(string.punctuation) + string.lowercase,
+)
+
 
 def get_words_from_string(line):
     """
@@ -78,12 +83,13 @@ def get_words_from_string(line):
     converting each word to lower-case.
 
     Input:  line (a string)
-    Output: a list of strings 
+    Output: a list of strings
               (each string is a sequence of alphanumeric characters)
     """
     line = line.translate(translation_table)
     word_list = line.split()
     return word_list
+
 
 ##############################################
 # Operation 3: count frequency of each word ##
@@ -95,10 +101,11 @@ def count_frequency(word_list):
     D = {}
     for new_word in word_list:
         if new_word in D:
-            D[new_word] = D[new_word]+1
+            D[new_word] = D[new_word] + 1
         else:
             D[new_word] = 1
     return D
+
 
 #############################################
 ## compute word frequencies for input file ##
@@ -112,20 +119,21 @@ def word_frequencies_for_file(filename):
     word_list = get_words_from_line_list(line_list)
     freq_mapping = count_frequency(word_list)
 
-    print "File",filename,":",
-    print len(line_list),"lines,",
-    print len(word_list),"words,",
-    print len(freq_mapping),"distinct words"
+    print("File", filename, ":")
+    print(len(line_list), "lines,")
+    print(len(word_list), "words,")
+    print(len(freq_mapping), "distinct words")
 
     return freq_mapping
 
-def inner_product(D1,D2):
+
+def inner_product(D1, D2):
     """
     Inner product between two vectors, where vectors
     are represented as dictionaries of (word,freq) pairs.
 
     Example: inner_product({"and":3,"of":2,"the":5},
-                           {"and":4,"in":1,"of":1,"this":2}) = 14.0 
+                           {"and":4,"in":1,"of":1,"this":2}) = 14.0
     """
     sum = 0.0
     for key in D1:
@@ -133,33 +141,33 @@ def inner_product(D1,D2):
             sum += D1[key] * D2[key]
     return sum
 
-def vector_angle(D1,D2):
+
+def vector_angle(D1, D2):
     """
     The input is a list of (word,freq) pairs, sorted alphabetically.
 
     Return the angle between these two vectors.
     """
-    numerator = inner_product(D1,D2)
-    denominator = math.sqrt(inner_product(D1,D1)*inner_product(D2,D2))
-    return math.acos(numerator/denominator)
+    numerator = inner_product(D1, D2)
+    denominator = math.sqrt(inner_product(D1, D1) * inner_product(D2, D2))
+    return math.acos(numerator / denominator)
+
 
 def main():
     if len(sys.argv) != 3:
-        print "Usage: docdist7.py filename_1 filename_2"
+        print("Usage: docdist7.py filename_1 filename_2")
     else:
         filename_1 = sys.argv[1]
         filename_2 = sys.argv[2]
         sorted_word_list_1 = word_frequencies_for_file(filename_1)
         sorted_word_list_2 = word_frequencies_for_file(filename_2)
-        distance = vector_angle(sorted_word_list_1,sorted_word_list_2)
-        print "The distance between the documents is: %0.6f (radians)"%distance
+        distance = vector_angle(sorted_word_list_1, sorted_word_list_2)
+        print(
+            "The distance between the documents is: {:.6f} (radians)".format(distance)
+        )
+
 
 if __name__ == "__main__":
     import profile
+
     profile.run("main()")
-
-    
-    
-
-
-
